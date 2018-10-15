a44cece97f80a19bcff50d2e19c557fc37278663
Isaac Yates <isaac.c.yates-1@ou.edu>
2018-10-15 12:38:45 -0500

    Mountains always appear, Percent land type params for Gamemap

--- gamemapping.py
+++ gamemapping.py
@@ -5,21 +5,11 @@ from entities import Entity
 
 squ_width = 3
 
-#set thresholds for terrain:
-water = (0.0, 0.52)
-grassland = (0.52, 0.7)
-mountain_low = (0.7, 0.75)
-mountain_mid = (0.75, 0.8)
-mountain_high = (0.8, 1.1)
-
-def within(val, tuple):
-        return val >= tuple[0] and val < tuple[1]
-
 #creates a noise map of values between 0.0 and 1.0
 def create_noise_map(size, octaves=5, max_val=1.0):
     value_map = [0.0 for i in range(size[0]*size[1])]
-    x_off = (random.randint(0, size[0]), random.randint(0, size[0]))
-    y_off = (random.randint(0, size[1]), random.randint(0, size[0]))
+    x_off = (random.randint(0, 100000), random.randint(0, 100000))
+    y_off = (random.randint(0, 100000), random.randint(0, 100000))
     for x in range(size[0]):
         for y in range(size[1]):
 
@@ -39,11 +29,23 @@ def create_noise_map(size, octaves=5, max_val=1.0):
 
 class GameMap(pygame.sprite.Sprite):
 
-    def __init__(self, size):
+    def __init__(self, size, percent_water=0.5, percent_land=0.3, percent_mountain=0.2):
         super().__init__()
         self.value_map = create_noise_map(size, octaves=6)
 
-        self.__create_mountains(size)
+        self.max_val = max(self.value_map)
+        self.min_val = min(self.value_map)
+
+        # 'max marks the value', AKA the threshold values mark the maximum possible value of each terrain type.
+        self.thresholds = [
+            [(self.max_val - self.min_val)*percent_water + self.min_val, "water"], 
+            [(self.max_val - self.min_val)*(percent_water + percent_land) + self.min_val, "grassland"], 
+            [(self.max_val - self.min_val)*(percent_water + percent_land + percent_mountain/3) + self.min_val, "mountain_low"], 
+            [(self.max_val - self.min_val)*(percent_water + percent_land + 2.3*percent_mountain/3) + self.min_val, "mountain_mid"], 
+            [self.max_val, "mountain_high"]
+        ]
+
+        print(self.thresholds, " min_val=", self.min_val, ", max_val=", self.max_val)
         
         self.width = size[0]*squ_width #width in pixels, not squares
         self.height = size[1]*squ_width
@@ -68,71 +70,18 @@ class GameMap(pygame.sprite.Sprite):
                 value = self.value_map[x*map_height + y]
                 rect = Rect(x*squ_width, y*squ_width, squ_width, squ_width)
                 
-                if within(value, water):
+                if self.get_land_type(value) == "water":
                     self.image.fill(colordefs.WATER, rect)
-                elif within(value, grassland):
+                elif self.get_land_type(value) == "grassland":
                     self.image.fill(colordefs.LOWLAND, rect)
-                elif within(value, mountain_low):
+                elif self.get_land_type(value) == "mountain_low":
                     self.image.fill(colordefs.MOUNTAIN_LOW, rect)
-                elif within(value, mountain_mid):
+                elif self.get_land_type(value) == "mountain_mid":
                     self.image.fill(colordefs.MOUNTAIN_MID, rect)
-                elif within(value, mountain_high):
+                elif self.get_land_type(value) == "mountain_high":
                     self.image.fill(colordefs.MOUNTAIN_HIGH, rect)
                 else:
                     self.image.fill(colordefs.RED, rect)
-
-    def __create_mountains(self, size):
-
-        def get_theta(vector):
-            return math.atan(vector[1]/vector[0])
-
-        def get_rand_search_vector(old_search_vector):
-            theta = get_theta(old_search_vector)
-            theta += random.randint(-45, 45)
-
-            return (math.cos(theta), math.sin(theta))
-        
-        def create_unit_vector(theta):
-            return (math.cos(theta), math.sin(theta))
-
-
-        def create_mountain(start_point, radius):
-            mountain_map = create_noise_map((2*radius, 2*radius), octaves=1)
-
-            for x in range(0, 2*radius):
-                for y in range(0, 2*radius):
-                    mountain_val =  mountain_map[x*2*radius + y]
-                    if mountain_val > 0.2 and math.sqrt((x - radius)**2 + (y - radius)**2) < radius:
-                        map_x = start_point[0] + x - radius
-                        map_y = start_point[1] + y - radius
-                        if map_x >= size[0] or map_x < 0 or map_y >=size[1] or map_y < 0:
-                            continue 
-                        map_val = self.value_map[map_x*size[1] + map_y]
-                        if map_val > water[1]:
-                            if mountain_val < 0.6:
-                                self.value_map[map_x*size[1] + map_y] = mountain_low[0]                    
-                            elif mountain_val < 0.65:
-                                self.value_map[map_x*size[1] + map_y] = mountain_mid[0]
-
-        gen_point = (random.randint(0, size[0]), random.randint(0, size[1]))
-        vector = (random.randint(1, 10), random.randint(1, 10))
-
-        for x in range(0, 11):
-            radius = random.randint(50, 80)
-            create_mountain(gen_point, radius)
-            vector = get_rand_search_vector(vector)
-            gen_point = (gen_point[0] + int(100*vector[0]), gen_point[1] + int(100.0*vector[1]))
-                    
-                    
-        # for theta in range(0, 360):
-        #     distance = 100.0*noise.pnoise1(theta/360)
-        #     search_vec = create_search_vector(theta)
-        #     print("d=", distance, " search_vec=", search_vec)
-        #     for d in range(0, int(distance)):
-        #         x = int(start_point[0] + d*search_vec[0])
-        #         y = int(start_point[1] + d*search_vec[1])
-        #         self.value_map[x*size[1] + y] = mountain_low[0]
-                
 
     def __add_trees(self):
         vmapWidth = int(self.width/squ_width)
@@ -148,14 +97,29 @@ class GameMap(pygame.sprite.Sprite):
                 if value < 0.0: value = 0.0
                 if value > 1.0: value = 1.0
 
-                if value > 0.4 and value < 0.7 and (within(self.value_map[x*vmapHeight + y], grassland) or within(self.value_map[x*vmapHeight + y], mountain_low)) and random.randint(0, 10000) > 9996: 
+                if value > 0.4 and value < 0.7 and (self.get_land_type(self.value_map[x*vmapHeight + y]) == "grassland" or self.get_land_type(self.value_map[x*vmapHeight + y]) == "mountain_low") and random.randint(0, 10000) > 9996: 
                     entity = Entity("TREE", (x*squ_width, y*squ_width), random.randint(10, 50))
                     entities.add_entity(entity) #this works janky 
                     self.drawEntity(entity)
-                elif value >= 0.7 and within(self.value_map[x*vmapHeight + y], grassland) and random.randint(0, 10000) > 9990: 
+                elif value >= 0.7 and self.get_land_type(self.value_map[x*vmapHeight + y]) == "grassland" and random.randint(0, 10000) > 9990: 
                     entity = Entity("TREE", (x*squ_width, y*squ_width), random.randint(20, 50))
                     entities.add_entity(entity) #this works janky 
                     self.drawEntity(entity)
+
+    def get_land_type(self, value):
+
+        if value < self.min_val or value > self.max_val:
+            raise Exception("Invalid terrain value: " + value.__str__())
+
+        #edge case for the following loop
+        if value < self.thresholds[0][0]:
+            return self.thresholds[0][1] #return water
+
+        for i in range(0, len(self.thresholds) - 1):
+            if value > self.thresholds[i][0] and value <= self.thresholds[i + 1][0]:
+                return self.thresholds[i + 1][1]
+
+        return None
 
     def __is_valid_pos(self, pos):
         x = int(pos[0] / squ_width)
